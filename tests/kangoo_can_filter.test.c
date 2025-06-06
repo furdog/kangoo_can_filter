15b2e3a0d0e6b2297206a8bcea686f8263a0b8c8
ElectroBoxUA <84919099+ElektroBOXua@users.noreply.github.com>
2025-06-06 15:15:10 +0300

    wrote tests

--- tests/kangoo_can_filter.test.c
+++ tests/kangoo_can_filter.test.c
@@ -32,6 +32,82 @@ void button_test()
 	assert(button_update(&b, 1)  == BUTTON_EVENT_NONE);
 }
 
+
+struct kangoo_fake_bms fbms;
+
+void fake_bms_test_start(struct kangoo_fake_bms *self)
+{
+	uint16_t i;
+	struct kangoo_fake_bms_can_frame frame;
+
+	kangoo_fake_bms_start(self);
+
+	frame.id = 0x423U;
+	frame.len = 1;
+	frame.data[0] = 0x07U;
+	kangoo_fake_bms_can_frame_write(self, frame);
+
+	/* Now test message reception from every frame */
+	for (i = 0; i < 9000; i++) {
+		kangoo_fake_bms_update(self, 1);
+
+		if ((i % 10) == 0 && i != 0) {		
+			assert(kangoo_fake_bms_can_frame_read(self) != NULL);
+			assert(kangoo_fake_bms_can_frame_read(self) == NULL);
+
+			/* Update immediately, so we can proccess other
+			 * message. */
+			kangoo_fake_bms_update(self, 0);
+		}
+
+		if ((i % 100) == 0 && i != 0) {
+			assert(kangoo_fake_bms_can_frame_read(self) != NULL);
+			assert(kangoo_fake_bms_can_frame_read(self) != NULL);
+			assert(kangoo_fake_bms_can_frame_read(self) != NULL);
+			assert(kangoo_fake_bms_can_frame_read(self) == NULL);
+
+			kangoo_fake_bms_update(self, 0);
+		}
+
+		if ((i % 3000) == 0 && i != 0) {
+			assert(kangoo_fake_bms_can_frame_read(self) != NULL);
+			assert(kangoo_fake_bms_can_frame_read(self) == NULL);
+		
+			kangoo_fake_bms_update(self, 0);
+		}
+	}
+}
+
+void fake_bms_test()
+{
+	uint16_t i;
+
+	kangoo_fake_bms_init(&fbms);
+
+	kangoo_fake_bms_update(&fbms, 0);
+
+	/* Bms not yet started, no messages should be received */
+	assert(kangoo_fake_bms_can_frame_read(&fbms) == NULL);
+
+	/* Not even after delay */
+	kangoo_fake_bms_update(&fbms, 300);
+	assert(kangoo_fake_bms_can_frame_read(&fbms) == NULL);
+
+	fake_bms_test_start(&fbms);
+
+	/* Now test stop */
+	kangoo_fake_bms_stop(&fbms);
+		
+	for (i = 0; i < 9000; i++) {
+		kangoo_fake_bms_update(&fbms, 1);
+
+		assert(kangoo_fake_bms_can_frame_read(&fbms) == NULL);
+	}
+
+	/* Test start again */
+	fake_bms_test_start(&fbms);
+}
+
 int main()
 {
 	button_init(&b);
@@ -39,6 +115,8 @@ int main()
 	/* Run two times to confirm behaviour. */
 	button_test();
 	button_test();
-	
+
+	fake_bms_test();
+
 	return 0;
 }
